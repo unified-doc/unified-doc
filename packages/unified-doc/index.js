e29cf619e4e62ee72e16c84456c120d0bb8691a6
chrisrzhou <chrisrzhou@pm.me>
2020-09-12 21:54:56 -0700

    fix(unified-doc): disable named exports until microbundle supports mixed exports

--- packages/unified-doc/index.js
+++ packages/unified-doc/index.js
@@ -1,3 +1,5 @@
 export { default } from './lib/doc';
 
-export * from './lib/doc';
+// TODO: support named exports after microbundle supports mixed exports
+// https://github.com/developit/microbundle/issues/712
+// export * from './lib/doc';
